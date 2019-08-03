36337b03d0f986729736e316c3c659511bfd9a32
loftkun <lofkun@gmail.com>
2019-08-03 18:58:46 +0900

    fix

--- test/compose.sh
+++ test/compose.sh
@@ -6,3 +6,19 @@ sudo rm volumes/mongo/db* -rf
 
 # launch
 sudo docker-compose up --build
+
+# launch standalone
+#sudo docker run -d \
+#    --name connpass-cron-search \
+#    --network connpasscronsearch_default \
+#    -e SEARCH_KEYWORD_OR="沖縄,okinawa" \
+#    -e SEARCH_ADDRESS_MATCHER="沖縄" \
+#    -e SEARCH_MAX_COUNT=3 \
+#    -e SEARCH_INTERVAL_SEC=10 \
+#    -e MONGO_HOST=store \
+#    -e MONGO_PORT=27017 \
+#    -e MONGO_DB=connpass-db \
+#    -e MONGO_USER=connpass-user \
+#    -e MONGO_PASS=1234 \
+#    -e MONGO_COLLECTION=events \
+#    loftkun/connpass-cron-search:latest
